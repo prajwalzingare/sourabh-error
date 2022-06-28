f50212c22677b0b3afb5594b4963ef738f49ebd1
prajwal zingare <prajwalzingre@gmail.com>
2022-06-28 12:06:42 +0530

    json

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,8 @@
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://admin-prajwal:prajwal12@cluster0.ox7k5.mongodb.net/todolistDB"
+    "mongodb+srv://admin-prajwal:prajwal12@cluster0.ox7k5.mongodb.net/todolistDB",
+    { useNewUrlParser: true }
   )
   .then(() => {
     console.log("Connection Succesful");
